0aa5a5eb49e9cecf0e31e77c6992a7fbc000c025
m1m4 <noamkadim@gmail.com>
2022-05-26 19:24:15 +0300

    added start method + documentation

--- prototype-coin/networking/peer.py
+++ prototype-coin/networking/peer.py
@@ -6,19 +6,17 @@ from json.decoder import JSONDecodeError
 
 import websockets
 from websockets.exceptions import *
+from typing import Optional, Union, Tuple, Any, Dict
 
-from networking.connection import PeerConnection
+from .connection import PeerConnection
 
 PORT = 11111
 IP = 'localhost'
 URI = f'ws://{IP}:{PORT}'
 
-loop = asyncio.get_event_loop()
 
-
-
-
-def server(func=None, *, webname=None):
+#TODO: rework
+def server(func=None, *, webname: Optional[str] = None):
     """A Server decorator. marks the decorated function as a web function,
     which lets the peer class register it as a valid server function for the
     handler
@@ -50,7 +48,7 @@ def server(func=None, *, webname=None):
     return wrapper
 
 
-def client(func=None, *, webname=None):
+def client(func=None, *, webname: Optional[str] = None):
     """A Client decorator. marks the decorated function as a web function,
     which lets the peer class register it as a valid client function for the
     handler. If no custom webname is giver 
@@ -77,38 +75,61 @@ class Peer:
     ALL = 0  # Choose all peers
     OUTBOUND = 1  # Chosse only outbound
     INBOUND = 2  # Choose only inbound
-    LAST = 3  # Choose last peer (not recommended)
 
     OKAY = 4  # Successfully replied to a get request
     GET = 5  # Get something
     POST = 6  # Post something
     ERROR = 7  # Sent whenever a get reqest has failed
 
-    def __init__(self) -> None:
+    def __init__(self, port: int = 11111,
+                 max_outbound: int = -1,
+                 max_inbound: int = -1) -> None:
+        """The peer class. Implements a peer that can send and listen to data.
+        Uses PeerConnection to handle connections. Data that is sent and 
+        received is in a dictionary in JSON format.
 
-        # Peers that are connected to us ie. client connections
-        self.inbound = set()
-        
-        # Peer that we are connected to ie. server connections
-        self.outbound = set()
-        
-        self.last_conn = None
-        
-        # Maximum amount of peers that can connect to us. -1 means no limit
-        self.max_outbound = -1
-        
-        # Maximum amount of peers we will connect to. -1 means no limit
-        self.max_inbound = -1
+
+        Attributes:
+            port (int, optional): The port of the peer. Defaults to 11111.
+
+            inbound (Set[PeerConnection]): A set of connections that are 
+            connnected to us. These conncections are client connections.
+
+            outbound (Set[PeerConnection]): A set of connections we are 
+            connected to. These are server connections that we can send requests
+            to.
+
+            commands (dict): The web commands.
+
+            max_outbound (int, optional): . Defaults to -1.
+            max_inbound (int, optional): _description_. Defaults to -1.
+
+            stop(asyncio.Event): Closes node when set
+
+
+        """
+
+        self.port = port
+
+        self.inbound = set()    # Peers that are connected to us
+        self.outbound = set()   # Peers that we are connected to
+
+        self.max_outbound = max_outbound
+        self.max_inbound = max_inbound
 
         self.commands = {}
+        self._assign_commands()
+
+        self.stop = asyncio.Event()
+
+    def _assign_commands(self):
 
         methods = []
-
-        cls = type(self)
+        this = type(self)
         # attribute is a string representing the attribute name
-        for attribute in dir(cls):
+        for attribute in dir(this):
             # Get the attribute value
-            attribute_value = getattr(cls, attribute)
+            attribute_value = getattr(this, attribute)
             # Check that it is callable
             if callable(attribute_value):
                 # Filter all dunder (__ prefix) methods
@@ -132,14 +153,46 @@ class Peer:
 
                 self.commands[method.webname] = ref
 
+            # Doesn't have an attribute of network command
             except AttributeError:
                 pass
-            
-    async def start(self, port):
-        self._server = await websockets.serve(self.init_connection, IP, port)
-        print(f'Server started. Listening on ws://{IP}:{port}')
 
-    async def init_connection(self, websocket):
+    async def start(self, *args):
+        """Starts the node. Set self.stop to stop the node.
+
+        Args:
+            port (int): Which port to open the node in
+        """
+
+        async with websockets.serve(self._init_connection, IP, self.port) as server:
+            asyncio.create_task(self._init_node(server, *args))
+            await self.stop.wait()
+
+            await self.disconnect(Peer.ALL)
+
+    async def _init_node(self, server, *args):
+        """The method which runs after the server is up. override to add more
+        functionality
+
+        Args:
+            server (websocket.WebSocketServer): The server instance
+        """
+        print(
+            f'Server started successfully. listening on ws://{IP}:{self.port}')
+
+    def find_conn(self, ip: int, port: int) -> PeerConnection:
+
+        addr = (ip, port)
+
+        conn = next((conn for conn in self.outbound if conn.addr == addr), None)
+
+        if conn is None:
+            conn = next((conn for conn in self.inbound if conn.addr == addr),
+                        None)
+
+        return conn
+
+    async def _init_connection(self, websocket):
         """Initiated whenever a peer wants to connect to us.
         Called by websockets.serve().
         """
@@ -147,16 +200,18 @@ class Peer:
         conn = PeerConnection(websocket)
         self.inbound.add(conn)
         # TODO: make sure the connection is working and valid before adding it
-        await conn.listener(handler=self.handler)
+        await conn.listener(handler=self._handler)
         self.inbound.remove(conn)
 
-    async def handler(self, data, connection):
+    async def _handler(self, data: Any, connection: PeerConnection):
         """The default handler function for each message for each connection.
-        override if needed.
+        data will only be proccessed if it was send in a dictionary in json 
+        format
 
         Args:
-            data (any): the data the was received.
-            connection (PeerConnection): The peer connection instance.
+            data (Any): The data that was received
+            connection (PeerConnection): The connection that we received the 
+            data from
         """
 
         print(f'INFO {connection.str_addr} - Received new message {data}')
@@ -165,12 +220,13 @@ class Peer:
             data = json.loads(data)
 
             datatype, body = data['type'], data['data']
-            
+
             # Check if got error
             if datatype == 'error':
-                print(f'WARNING {connection.str_addr} - Received error: {body["message"]}')
+                print(
+                    f'WARNING {connection.str_addr} - Received error: {body["message"]}')
                 return
-            
+
             command_name = body['command']
 
             body_temp = copy.deepcopy(body)
@@ -178,85 +234,81 @@ class Peer:
             command_params = body_temp
             command = self.commands[command_name][0]
 
-            # TODO: pass paramater as normal arguments instead of one big dictionary
-            if datatype == 'get':
-                response = await command(command_params)
-            if datatype == 'post':
-                response = await command(connection, command_params)
-
-            if not response is None:
-                await connection.send(response)
-
         except JSONDecodeError:
             print(f'{connection.str_addr}: ERROR - not in json format')
-            await connection.send(self.pack(Peer.ERROR),
-                                  {'message': 'not in json format'})
+            await connection.send(self.pack(Peer.ERROR,
+                                  {'message': 'not in json format'}))
             return
 
-        except KeyError:
+        except KeyError as e:
             print(f'{connection.str_addr}: ERROR - wrong format')
             await connection.send(self.pack(Peer.ERROR,
                                   {'message': 'wrong format'}))
             return
-        
-        except TypeError:
-            
+
+        except TypeError as e:
+
             if data is None:
                 print(f'{connection.str_addr}: INFO - got empty message')
-            print(f'{connection.str_addr}: ERROR - data must be a dictionary')
+
+            print(f'{connection.str_addr}: ERROR - {e}')
             await connection.send(self.pack(Peer.ERROR,
                                   {'message': 'data must be a dictionary'}))
 
-        # except AttributeError as e:
-        #     exc_type, exc_obj, exc_tb = sys.exc_info()
-        #     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        #     print(f'ERROR: {e} at {fname} in line {exc_tb.tb_lineno}')
-        #     self.default(data)
+        if datatype == 'get':
+            response = await command(command_params)
+        if datatype == 'post':
+            response = await command(connection, command_params)
 
-    async def connect(self, addr):
-        """Connect to a peer
+        if not response is None:
+            await connection.send(response)
+
+    async def connect(self, addr: Union[str, Tuple[str, int]]):
+        """Connects to a peer
 
         Args:
-            addr (str/(str, int)): The address of the peer we want to connect
+            addr (Union[str, Tuple[str, int]): The address of the peer we want to connect
             to. can be either a uri or a tuple containing the ip and the port
-
-        Returns:
-            _type_: _description_
         """
 
-        if isinstance(addr, str):
-            addr = addr.split(':')
-            if len(addr) != 3:
-                print('ERROR - Tried to connect to an invalid address')
-
-            addr = (addr[1][2:], addr[2])
-
-        uri = f'ws://{addr[0]}:{addr[1]}'
+        if isinstance(addr, tuple):
+            uri = f'ws://{addr[0]}:{addr[1]}'
+        else:
+            uri = addr
 
         # Check if this connection already exits
         for node in self.outbound:
-            if node.addr == addr:
+            node_uri = f'ws://{node.addr[0]}:{node.addr[1]}'
+            if node_uri == uri.replace("localhost", "127.0.0.1"):
                 print(f'ERROR - already conncted to {node.str_addr}')
                 return
 
-        client = await websockets.connect(uri)
+        try:
+            client = await websockets.connect(uri)
+
+        except ConnectionRefusedError:
+            print(f'ERROR - {uri} refused connection ')
+            return
+
+        except InvalidURI:
+            print(f'ERROR - "{uri}" is not a valid uri')
+            return
+
         conn = PeerConnection(client, connected=False)
         self.outbound.add(conn)
-        self.last_conn = conn
 
         return conn
 
-    async def disconnect(self, peer_conn):
+    async def disconnect(self, peer_conn: Union[PeerConnection, int]):
         """Disconnect from a peer
 
         Args:
-            peer_conn (PeerConnection/int): The peer's connection. enter one of the 
+            peer_conn (Union[PeerConnection, int]): The peer's connection. enter one of the 
             codes below to easily disonnect from peers:
 
-            0 - Disconnect from all peers
-            1 - Disconnect from all outbound peers
-            2 - Disconnect from all inbound peers
-            3 - Disconnect from last peer
+            Peer.ALL - Disconnect from all peers
+            Peer.OUTBOUND - Disconnect from all outbound peers
+            Peer.INBOUND - Disconnect from all inbound peers
         """
 
         if isinstance(peer_conn, int):
@@ -284,9 +336,6 @@ class Peer:
 
                     self.inbound = set()
 
-                case Peer.LAST:
-                    return await self.disconnect(self.last_conn)
-
                 case _:
                     return False
         else:
@@ -303,8 +352,8 @@ class Peer:
 
         return True
 
-    async def recvall(self, return_when=None, timeout=2):
-        """recieves from all connections and blocks the the function untill
+    async def recvall(self, return_when: Optional[str] = None, timeout: int = 3):
+        """receives from all connections and blocks the the function untill
         received
 
         Args:
@@ -326,10 +375,13 @@ class Peer:
 
         # Take only the tasks to pass it to async.wait()
         tasks = [task[1] for task in tasks_full]
-        
-        done, pending = await asyncio.wait(tasks, return_when=return_when,
-                                           timeout=timeout)
-        
+
+        try:
+            done, pending = await asyncio.wait(tasks, return_when=return_when,
+                                               timeout=timeout)
+        except ValueError:
+            return
+
         # Cancel all requests that didn't reponse within the specified timeout
         for task in pending:
             task.cancel()
@@ -338,16 +390,16 @@ class Peer:
             # Find the peer that answered first
             for request in tasks_full:
                 if request[1] in done:
-                    return request[0], json.loads(request[1].result())
+                    return request[0], request[1].result()
         else:
             results = []
             for request in tasks_full:
                 if request[1] in done:
-                    results.append((request[0], json.loads(request[1].result())))
+                    results.append((request[0], request[1].result()))
 
             return results
 
-    async def broadcast(self, data, raw=False, wait=False):
+    async def broadcast(self, data: Any, raw: bool = False, wait: bool = False):
         """Broadcasts a message to all known peers
 
         Args:
@@ -360,14 +412,38 @@ class Peer:
         # coros += [peer.send(data, raw) for peer in self.inbound]
         await asyncio.gather(*coros)
 
-    def connections(self):
+    def connections(self) -> int:
+        """Returns the number on online connections
+
+        Returns:
+            int: The number of connections
+        """
         return len(self.inbound) + len(self.outbound)
 
     @server
     def default(self, data):
+        """The default command to handle incoming messages
+
+        Args:
+            data (dict): the data
+        """
         print(f'No command is found. message: {data}')
 
-    def pack(self, datatype, data):
+    def pack(self, datatype: int, data: Any) -> dict:
+        """Packs the data to be ready to send over the network.
+
+        Args:
+            datatype (int): The data type. types are as follows: 
+            Peer.OKAY - The request was received correctly. No error occured
+            Peer.GET - Request some data and expect a response
+            Peer.POST - Broadcast data and not expect a response
+            Peer.ERROR - An error has occured
+
+            data (Any): The data to pack
+
+        Returns:
+            dict: The data to send
+        """
 
         match datatype:
             case Peer.OKAY:
@@ -401,8 +477,7 @@ if __name__ == '__main__':
         asyncio.run(main())
 
     except KeyboardInterrupt:
-        print('Shutting Down...')
-        loop.close()
+        pass
 
 # Lazy client:
 # py -m websockets ws://localhost:22222
